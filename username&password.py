e7c91bc6c1b2def49057b05c2b06fd47099dd54d
melihteke <36086368+melihteke@users.noreply.github.com>
2018-02-02 21:32:28 +0100

    Update username&password.py

--- username&password.py
+++ username&password.py
@@ -4,7 +4,7 @@ typed_pass=input("Please enter your 4 digit code\n\n")
 database_password = "1234"
 database_username = "kemal"
 
-if typed_username != database_username or typed_pass != database_username:
+if typed_username != database_username or typed_pass != database_password:
     print("Wrong username or password, please try again.\n")
 else:
     print("Congratulations!!, Now you are authrized to have access\n\n")
